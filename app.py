ed4395599312128c676d748593503c6535ee558e
bzmdqz <114287249+bzmdqz@users.noreply.github.com>
2022-10-15 17:17:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def zhixinghanshu():  # 代码放这
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=true)
