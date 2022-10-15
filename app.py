470ec67ff0e8be833e2ca66dc0644ccca56a5f83
bzmdqz <114287249+bzmdqz@users.noreply.github.com>
2022-10-15 17:38:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def zhixinghanshu():  # 代码放这
+def zhixinghanshu():  # 代码放这(分类)
     return render_template("index.html")
+
+
+@app.route('/')   #等前端写完
+def zhuce():
+    
 
 
 if __name__ == '__main__':
